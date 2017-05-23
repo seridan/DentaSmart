992809d3b4030d5ed6e08fd67b1e208b46e543a1
Seridan Viera <Seridan Viera>
2017-05-23 18:30:04 +0100

    Ajustada Vbox  hoja de estilo para que se vea el selector de fechas del datepicker

--- es/dentasmart/controlador/FXMLRegistroPacienteController.java
+++ es/dentasmart/controlador/FXMLRegistroPacienteController.java
@@ -1,8 +1,81 @@
 package es.dentasmart.controlador;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXTimePicker;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by seridan on 16/05/2017.
  */
-public class FXMLRegistroPacienteController {
+public class FXMLRegistroPacienteController implements Initializable {
 
+    @FXML
+    private JFXTextField dniTxt;
+
+    @FXML
+    private JFXTextField nombreTxt;
+
+    @FXML
+    private JFXTextField apellido1Txt;
+
+    @FXML
+    private JFXTextField apellido2Txt;
+
+    @FXML
+    private JFXTextField direccionTxt;
+
+    @FXML
+    private JFXTextField localidad2;
+
+    @FXML
+    private JFXTextField codPostalTxt2;
+
+    @FXML
+    private JFXTextField tfnoFijoTxt2;
+
+    @FXML
+    private JFXTextField tfnoMovilTxt2;
+
+    @FXML
+    private JFXTextField emailTxt2;
+
+    @FXML
+    private JFXDatePicker fechaNacTxt;
+
+    @FXML
+    private JFXTextField localidad;
+
+    @FXML
+    private JFXTextField codPostalTxt;
+
+    @FXML
+    private JFXTextField tfnoFijoTxt;
+
+    @FXML
+    private JFXButton btnGuardar;
+
+    @FXML
+    private JFXButton btnEditar;
+
+    @FXML
+    private JFXButton btnLimpiar;
+
+    @FXML
+    private JFXDatePicker fechaCitaTxt;
+
+    @FXML
+    private JFXDatePicker horaCitaTxt;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        
+
+    }
 }
